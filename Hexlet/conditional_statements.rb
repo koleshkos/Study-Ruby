66340b647355f4c23bdeac1bac93ded61daa6dce
Sergey <koleshkoser@gmail.com>
2020-01-30 20:25:34 +0300

    add conditional statements

---
+++ Hexlet/conditional_statements.rb
@@ -0,0 +1,38 @@
+x = 8
+y = 12
+
+if x<y
+  puts ("X меньше Y")
+end
+
+isSmall = true
+
+if isSmall
+  puts "is Small = #{isSmall}"
+end
+
+isSmall = false
+
+if isSmall
+  puts "is Small = #{isSmall}"
+end
+
+
+if x>0 and y<20
+  puts "OK"
+end
+
+if x>0 and y>20
+  puts "OK"
+else
+  puts "Not OK"
+end
+
+
+if x == y
+  put "x=y"
+elsif x > y
+  puts "x>y"
+else
+  puts "x<y"
+end        
